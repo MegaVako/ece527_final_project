645082677335182724a9c3e16ad9407755da7716
Bill Dai <billdai2@illinois.edu>
2022-12-13 18:53:12 +0000

    some opt, around 40k cycle, ultilization LUT ~161%

--- lstm.cpp
+++ lstm.cpp
@@ -30,61 +30,79 @@ t_feature tanh(t_feature input)
         return tanh_lut[(int)(input + 4) * 1024];
 }
 
-void input_to_hidden_dot_product(t_feature x[INPUT_FEATURE_D], t_feature W[INPUT_FEATURE_D][HIDDEN_FEATURE_D], t_feature output[HIDDEN_FEATURE_D])
+#define I_TO_H_UNROLL_RATIO 8
+void input_to_hidden_dot_product(t_feature x[INPUT_FEATURE_D], t_weight W[INPUT_FEATURE_D][HIDDEN_FEATURE_D], t_feature output[HIDDEN_FEATURE_D])
 {
-    for (int i = 0; i < INPUT_FEATURE_D; ++i)
-    {
-        for (int j = 0; j < HIDDEN_FEATURE_D; ++j)
-        {
-            output[j] += x[i] * W[i][j];
+	I_TO_H : for (int i = 0; i < INPUT_FEATURE_D; ++i)
+	{
+#pragma HLS PIPELINE
+		I_TO_2 : for (int j = 0; j < HIDDEN_FEATURE_D / I_TO_H_UNROLL_RATIO; ++j)
+		{
+    		I_TO_H_3 : for (int k = 0; k < I_TO_H_UNROLL_RATIO; k++) {
+#pragma HLS UNROLL
+    			output[j*I_TO_H_UNROLL_RATIO + k] += x[i] * W[i][j*I_TO_H_UNROLL_RATIO + k];
+    		}
         }
     }
 }
 
-void hidden_to_hidden_dot_product(t_feature h[HIDDEN_FEATURE_D], t_feature U[HIDDEN_FEATURE_D][HIDDEN_FEATURE_D], t_feature output[HIDDEN_FEATURE_D])
+#define H_TO_H_UNROLL_RATIO 8
+void hidden_to_hidden_dot_product(t_feature h[HIDDEN_FEATURE_D], t_weight U[HIDDEN_FEATURE_D][HIDDEN_FEATURE_D], t_feature output[HIDDEN_FEATURE_D])
 {
-    for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
+	H_TO_H : for (int j = 0; j < HIDDEN_FEATURE_D; ++j)
     {
-        for (int j = 0; j < HIDDEN_FEATURE_D; ++j)
+#pragma HLS PIPELINE
+		H_TO_H_2 : for (int i = 0; i < HIDDEN_FEATURE_D / H_TO_H_UNROLL_RATIO; ++i)
         {
-            output[i] += h[i] * U[i][j];
+			H_TO_H_3 : for (int k = 0; k < H_TO_H_UNROLL_RATIO; k++) {
+#pragma HLS UNROLL
+	            output[i*H_TO_H_UNROLL_RATIO + k] += h[i*H_TO_H_UNROLL_RATIO + k] * U[i][j];
+			}
         }
     }
 }
 /*
     input gate/ forget gate/ output gate share the same function
 */
-void unified_gate(t_feature W[INPUT_FEATURE_D][HIDDEN_FEATURE_D], t_feature x[INPUT_FEATURE_D],
-                  t_feature U[HIDDEN_FEATURE_D][HIDDEN_FEATURE_D], t_feature h[HIDDEN_FEATURE_D],
-                  t_feature bias[HIDDEN_FEATURE_D],
+void unified_gate(t_weight W[INPUT_FEATURE_D][HIDDEN_FEATURE_D], t_feature x[INPUT_FEATURE_D],
+		t_weight U[HIDDEN_FEATURE_D][HIDDEN_FEATURE_D], t_feature h[HIDDEN_FEATURE_D],
+		t_weight bias[HIDDEN_FEATURE_D],
                   t_feature output[HIDDEN_FEATURE_D])
 {
     t_feature output_x[HIDDEN_FEATURE_D];
     t_feature output_h[HIDDEN_FEATURE_D];
 
+#pragma HLS array_partition variable = output_h cyclic factor = 8
+#pragma HLS array_partition variable = output_x cyclic factor = 8
+
     input_to_hidden_dot_product(x, W, output_x);
     hidden_to_hidden_dot_product(h, U, output_h);
 
-    for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
+    UNI_GATE : for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
     {
+#pragma HLS UNROLL factor = 8
         t_feature tmp_sum = output_x[i] + output_h[i] + bias[i];
         output[i] = sigmoid(tmp_sum);
     }
 }
 
-void g_gate(t_feature W[INPUT_FEATURE_D][HIDDEN_FEATURE_D], t_feature x[INPUT_FEATURE_D],
-                t_feature U[HIDDEN_FEATURE_D][HIDDEN_FEATURE_D], t_feature h[HIDDEN_FEATURE_D],
-                t_feature bias[HIDDEN_FEATURE_D],
+void g_gate(t_weight W[INPUT_FEATURE_D][HIDDEN_FEATURE_D], t_feature x[INPUT_FEATURE_D],
+		t_weight U[HIDDEN_FEATURE_D][HIDDEN_FEATURE_D], t_feature h[HIDDEN_FEATURE_D],
+		t_weight bias[HIDDEN_FEATURE_D],
                 t_feature output[HIDDEN_FEATURE_D])
 {
     t_feature output_x[HIDDEN_FEATURE_D];
     t_feature output_h[HIDDEN_FEATURE_D];
 
+#pragma HLS array_partition variable = output_h cyclic factor = 8
+#pragma HLS array_partition variable = output_x cyclic factor = 8
+
     input_to_hidden_dot_product(x, W, output_x);
     hidden_to_hidden_dot_product(h, U, output_h);
 
-    for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
+    G_GATE : for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
     {
+#pragma HLS UNROLL factor = 8
         t_feature tmp_sum = output_x[i] + output_h[i] + bias[i];
         output[i] = tanh(tmp_sum);
     }
@@ -93,16 +111,18 @@ void g_gate(t_feature W[INPUT_FEATURE_D][HIDDEN_FEATURE_D], t_feature x[INPUT_FE
 void cell_out(t_feature forget[HIDDEN_FEATURE_D], t_feature last_c[HIDDEN_FEATURE_D], t_feature input_t[HIDDEN_FEATURE_D], t_feature g_gate[HIDDEN_FEATURE_D],
                t_feature output[HIDDEN_FEATURE_D])
 {
-    for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
+    CELL_OUT : for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
     {
+#pragma HLS UNROLL
         output[i] = forget[i] * last_c[i] + input_t[i] * g_gate[i];
     }
 }
 
 void next_hidden(t_feature output_t[HIDDEN_FEATURE_D], t_feature cell_out_t[HIDDEN_FEATURE_D], t_feature output[HIDDEN_FEATURE_D])
 {
-    for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
+	NEXT_HIDDEN : for (int i = 0; i < HIDDEN_FEATURE_D; ++i)
     {
+#pragma HLS UNROLL
         output[i] = output_t[i] * tanh(cell_out_t[i]);
     }
 }
@@ -131,10 +151,11 @@ static void LSTM_cell(t_feature input[INPUT_FEATURE_D],
     t_feature output_g[HIDDEN_FEATURE_D];
     t_feature output_o[HIDDEN_FEATURE_D];
 
-//    unified_gate(t_feature W[INPUT_FEATURE_D][HIDDEN_FEATURE_D], t_feature x[INPUT_FEATURE_D],
-//                      t_feature U[HIDDEN_FEATURE_D][HIDDEN_FEATURE_D], t_feature h[HIDDEN_FEATURE_D],
-//                      t_feature bias[HIDDEN_FEATURE_D],
-//                      t_feature output[HIDDEN_FEATURE_D])
+#pragma HLS array_partition variable = output_i cyclic factor = 8
+#pragma HLS array_partition variable = output_f cyclic factor = 8
+#pragma HLS array_partition variable = output_g cyclic factor = 8
+#pragma HLS array_partition variable = output_o cyclic factor = 8
+
     // input gate
     unified_gate(weight_matrix_xi, input, weight_matrix_hi, hidden_state, bias_i, output_i);
     // forget gate
@@ -150,7 +171,7 @@ static void LSTM_cell(t_feature input[INPUT_FEATURE_D],
 }
 
 // wrapper lstm
-
+#define CELL_UNROLL_RATIO 2
 int wrapper_flow(t_feature input_seq[INPUT_SEQUENTIAL_D][INPUT_FEATURE_D],
                  t_feature hidden_state_seq[INPUT_SEQUENTIAL_D][HIDDEN_FEATURE_D],
                  t_weight weight_matrix_hf[HIDDEN_FEATURE_D][HIDDEN_FEATURE_D],
@@ -170,21 +191,21 @@ int wrapper_flow(t_feature input_seq[INPUT_SEQUENTIAL_D][INPUT_FEATURE_D],
     t_feature cell_state_seq[INPUT_SEQUENTIAL_D][HIDDEN_FEATURE_D];
     t_feature dummy_hidden[32] = {0};
     t_feature dummy_cell[32] = {0};
-//#pragma HLS dataflow
-#pragma HLS array_partition variable = weight_matrix_hf cyclic dim = 2 factor = 32
-#pragma HLS array_partition variable = weight_matrix_xf cyclic dim = 2 factor = 32
-#pragma HLS array_partition variable = weight_matrix_hi cyclic dim = 2 factor = 32
-#pragma HLS array_partition variable = weight_matrix_xi cyclic dim = 2 factor = 32
-#pragma HLS array_partition variable = weight_matrix_ho cyclic dim = 2 factor = 32
-#pragma HLS array_partition variable = weight_matrix_xo cyclic dim = 2 factor = 32
-#pragma HLS array_partition variable = weight_matrix_hg cyclic dim = 2 factor = 32
-#pragma HLS array_partition variable = weight_matrix_xg cyclic dim = 2 factor = 32
-#pragma HLS array_partition variable = hidden_state_seq cyclic dim = 2 factor = 32
 
-#pragma HLS array_partition variable = bias_f complete factor = 32
-#pragma HLS array_partition variable = bias_i complete factor = 32
-#pragma HLS array_partition variable = bias_o complete factor = 32
-#pragma HLS array_partition variable = bias_c complete factor = 32
+#pragma HLS array_partition variable = weight_matrix_hf cyclic dim = 2 factor = 8
+#pragma HLS array_partition variable = weight_matrix_xf cyclic dim = 2 factor = 8
+#pragma HLS array_partition variable = weight_matrix_hi cyclic dim = 2 factor = 8
+#pragma HLS array_partition variable = weight_matrix_xi cyclic dim = 2 factor = 8
+#pragma HLS array_partition variable = weight_matrix_ho cyclic dim = 2 factor = 8
+#pragma HLS array_partition variable = weight_matrix_xo cyclic dim = 2 factor = 8
+#pragma HLS array_partition variable = weight_matrix_hg cyclic dim = 2 factor = 8
+#pragma HLS array_partition variable = weight_matrix_xg cyclic dim = 2 factor = 8
+#pragma HLS array_partition variable = hidden_state_seq cyclic dim = 2 factor = 8
+
+#pragma HLS array_partition variable = bias_f cyclic factor = 8
+#pragma HLS array_partition variable = bias_i cyclic factor = 8
+#pragma HLS array_partition variable = bias_o cyclic factor = 8
+#pragma HLS array_partition variable = bias_c cyclic factor = 8
 
     LSTM_cell(
         input_seq[0],
@@ -205,27 +226,49 @@ int wrapper_flow(t_feature input_seq[INPUT_SEQUENTIAL_D][INPUT_FEATURE_D],
         hidden_state_seq[0],  // next
         cell_state_seq[0]    // next
     );
-    for (int i = 1; i < INPUT_SEQUENTIAL_D; i++) {
-#pragma hls UNROLL
-        LSTM_cell(
-            input_seq[i],
-            hidden_state_seq[i-1],
-            cell_state_seq[i-1],
-            weight_matrix_hf,
-            weight_matrix_xf,
-            weight_matrix_hi,
-            weight_matrix_xi,
-            weight_matrix_ho,
-            weight_matrix_xo,
-            weight_matrix_hg,
-            weight_matrix_xg,
-            bias_f,
-            bias_i,
-            bias_o,
-            bias_c,
-            hidden_state_seq[i],  // next
-            cell_state_seq[i]    // next
-        );
+    LSTM_cell(
+        input_seq[1],
+		dummy_hidden,
+		dummy_cell,
+        weight_matrix_hf,
+        weight_matrix_xf,
+        weight_matrix_hi,
+        weight_matrix_xi,
+        weight_matrix_ho,
+        weight_matrix_xo,
+        weight_matrix_hg,
+        weight_matrix_xg,
+        bias_f,
+        bias_i,
+        bias_o,
+        bias_c,
+        hidden_state_seq[2],  // next
+        cell_state_seq[2]    // next
+    );
+    CELL_LOOP_OUTER : for (int j = 1; j < INPUT_SEQUENTIAL_D / CELL_UNROLL_RATIO; j++) {
+		CELL_LOOP_INNER : for (int i = 0; i < CELL_UNROLL_RATIO; i++) {
+			int curr_idx = i + (j*CELL_UNROLL_RATIO) - 1;
+			int next_idx = curr_idx + 1;
+			LSTM_cell(
+				input_seq[curr_idx],
+				hidden_state_seq[curr_idx],
+				cell_state_seq[curr_idx],
+				weight_matrix_hf,
+				weight_matrix_xf,
+				weight_matrix_hi,
+				weight_matrix_xi,
+				weight_matrix_ho,
+				weight_matrix_xo,
+				weight_matrix_hg,
+				weight_matrix_xg,
+				bias_f,
+				bias_i,
+				bias_o,
+				bias_c,
+				hidden_state_seq[next_idx],  // next
+				cell_state_seq[next_idx]    // next
+			);
+		}
     }
     return 0;
 }
